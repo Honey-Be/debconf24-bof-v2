bb5908260aeb94f6d1df0f3be551f8c45de1e2c5
윤병익 <yeun0908@gmail.com>
2024-07-28 17:06:56 +0900

    fix

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,14 +1,10 @@
 import { QwikAuth$ } from "@auth/qwik";
-import DebianSalsa from './../utils/salsa';
+import DebianSalsa from './salsa';
 import Github from "@auth/qwik/providers/github"
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
-    providers: [Github({
-      clientId: import.meta.env.GITHUB_ID,
-      clientSecret: import.meta.env.GITHUB__CLIENT_SECRET
-    }),
-    DebianSalsa({
+    providers: [DebianSalsa({
       clientId: import.meta.env.DEBIAN_SALSA_CLIENT_ID,
       clientSecret: import.meta.env.DEBIAN_SALSA_CLIENT_SECRET,
     })],
