357745faec0ecb0d263416e9fd2250fbd7f5a0b7
윤병익 <yeun0908@gmail.com>
2024-07-28 19:58:59 +0900

    fix

--- src/components/widgets/AuthPanel.tsx
+++ src/components/widgets/AuthPanel.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
+import { Form, Link } from "@builder.io/qwik-city";
 import { useSession, useSignIn, useSignOut } from "~/routes/plugin@auth";
 
 export default component$(() => {
@@ -12,19 +12,18 @@ export default component$(() => {
             <div class="hover:text-link dark:hover:text-white px-4 py-3 flex items-centers">
                 {session.value.user?.email}
             </div>
-            <Link
-                class="btn btn-primary ml-2 py-2.5 px-5.5 md:px-6 font-semibold shadow-none text-sm w-auto"
-                onClick$={() => {signOut.submit({})}}>
-                Logout
-            </Link>
+
+            <Form action={signOut}>
+                <input type="hidden" name="redirectTo" value="/" />
+                <button class="btn btn-primary ml-2 py-2.5 px-5.5 md:px-6 font-semibold shadow-none text-sm w-auto">Logout</button>
+            </Form>
         </>
     } else {
         return <>
-            <Link
-                class="btn btn-primary ml-2 py-2.5 px-5.5 md:px-6 font-semibold shadow-none text-sm w-auto"
-                onClick$={() => {signIn.submit({providerId: "debian-salsa"})}}>
-                Login
-            </Link>
+            <Form action={signIn}>
+                <input type="hidden" name="providerId" value="debian-salsa" />
+                <button class="btn btn-primary ml-2 py-2.5 px-5.5 md:px-6 font-semibold shadow-none text-sm w-auto">Login</button>
+            </Form>
         </>
     }
     
