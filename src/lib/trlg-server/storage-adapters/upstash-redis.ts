a99b9f144497166d107d05052710626ff47ae326
윤병익 <yeun0908@gmail.com>
2024-07-28 18:22:00 +0900

    fix

--- src/lib/trlg-server/storage-adapters/upstash-redis.ts
+++ src/lib/trlg-server/storage-adapters/upstash-redis.ts
@@ -9,8 +9,8 @@ const jsonPool: {
 } = {}
 
 const redis = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN
+    url: import.meta.env.UPSTASH_REDIS_REST_URL,
+    token: import.meta.env.UPSTASH_REDIS_REST_TOKEN
 })
 
 export async function getGameIdList() {
--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -4,11 +4,16 @@ import Github from "@auth/qwik/providers/github"
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
-    secret: process.env.AUTH_SECRET,
+    secret: import.meta.env.AUTH_SECRET,
     trustHost: true,
     providers: [DebianSalsa({
-      clientId: process.env.DEBIAN_SALSA_CLIENT_ID,
-      clientSecret: process.env.DEBIAN_SALSA_CLIENT_SECRET,
+      clientId: import.meta.env.DEBIAN_SALSA_CLIENT_ID,
+      clientSecret: import.meta.env.DEBIAN_SALSA_CLIENT_SECRET,
     })],
+    callbacks: {
+      async redirect({url, baseUrl}) {
+          return url
+      },
+    }
   }),
 );
