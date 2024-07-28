adb06c43790285a56abd89a6ea6c13371120773c
윤병익 <yeun0908@gmail.com>
2024-07-28 17:36:27 +0900

    fix

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -6,7 +6,7 @@ export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
     providers: [DebianSalsa({
       clientId: process.env.DEBIAN_SALSA_CLIENT_ID,
-      clientSecret: process..env.DEBIAN_SALSA_CLIENT_SECRET,
+      clientSecret: process.env.DEBIAN_SALSA_CLIENT_SECRET,
     })],
   }),
 );
