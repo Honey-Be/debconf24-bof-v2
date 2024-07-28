fcbff2ac243a24b17abf420930bba755fa54d452
윤병익 <yeun0908@gmail.com>
2024-07-28 17:36:08 +0900

    fix

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -5,8 +5,8 @@ import Github from "@auth/qwik/providers/github"
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
     providers: [DebianSalsa({
-      clientId: import.meta.env.DEBIAN_SALSA_CLIENT_ID,
-      clientSecret: import.meta.env.DEBIAN_SALSA_CLIENT_SECRET,
+      clientId: process.env.DEBIAN_SALSA_CLIENT_ID,
+      clientSecret: process..env.DEBIAN_SALSA_CLIENT_SECRET,
     })],
   }),
 );
