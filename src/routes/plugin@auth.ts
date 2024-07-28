6ddf322e9bb238decc2e76a022efde592407f100
윤병익 <yeun0908@gmail.com>
2024-07-28 21:02:32 +0900

    fix

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,14 +1,11 @@
 import { QwikAuth$ } from "@auth/qwik";
-import DebianSalsa from './salsa';
-import Github from "@auth/qwik/providers/github"
+import DebianSalsa from "./salsa";
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
-  () => ({
-    secret: process.env.AUTH_SECRET,
-    trustHost: true,
+  ({env}) => ({
     providers: [DebianSalsa({
-      clientId: process.env.DEBIAN_SALSA_CLIENT_ID,
-      clientSecret: process.env.DEBIAN_SALSA_CLIENT_SECRET,
-    })]
+      clientId: env.get("DEBIAN_SALSA_CLIENT_ID"),
+      clientSecret: env.get("DEBIAN_SALSA_CLIENT_SECRET"),
+    })],
   }),
 );
